2a381fb0fb5df7f3e943cbfc176be74b34d89efc
Sabrina Markon <phpsitescripts@outlook.com>
2018-12-10 23:58:48 -0700

    Add push method for linked list

--- php/classes/LinkedList.php
+++ php/classes/LinkedList.php
@@ -6,7 +6,7 @@ class Node {
     private $data = null;
     private $next = null;
 
-    public function __construct($value) {
+    public function __construct($value = false) {
 
         $this->data = $value;
         $this->next = null;
@@ -21,7 +21,39 @@ class Node {
 
 class LinkedList {
 
-    
+    private $head = null;
+    private $tail = null;
+    private $length = 0;
 
+    public function push($item) {
+
+        if ($head === null) {
+
+            $this->head = $node;
+            $this->tail = $node;
+            $this->length++;
+            return $node;
+        }
+        $this->$node = new Node($value);
+        $this->tail = $node;
+        $this->length++;
+
+        return $node;
+    }
+
+    public function pop() {
+
+
+    }
+
+    public function length() {
+
+
+    }
+
+    public function isEmpty() {
+
+        return $this->length === 0;
+    }
 
 }
