7e7c5de0337f87d96539bc280da8aae84e2fa802
Sabrina Markon <phpsitescripts@outlook.com>
2018-12-12 00:27:47 -0700

    While loop for finding second to last node in linked list in order to reassign it as tail node

--- php/classes/LinkedList.php
+++ php/classes/LinkedList.php
@@ -46,20 +46,67 @@ class LinkedList {
     /* pop the item at the end of the list off */
     public function pop() {
 
+        // this linked list is already empty.
         if ($this->isEmpty()) {
 
             return null;
+
         }
 
-        const node = $this->tail;
-
+        // if there are any nodes, we will return the tail node.
+        $node = $this->tail;
+        
+        // this linked list only had one node so tail and head are the same node.
         if ($this->head === $this->tail) {
 
             $this->head = null;
             $this->tail = null;
             $this->length--;
-            return node;
+
+            return $node;
+
         }
+
+        /* remove the tail from the linked list, then change the second-to-last node's 
+        next value to null, then change the second-to-last node itself to be the tail. This is
+        the inefficiency of linked lists, because to find a node, such as the second-to-last, we have
+        to go through the linked list node by node until we find it O(n) */
+        
+        // SO:
+        $current = $this->head; // the one we are looking at for this iteration in the loop.
+        $secondtolast; // the second-to-last node in the linked list.
+
+        while ($current) {
+
+            if ($current->next === $this->tail) {
+
+                // means we know we are on the second-to-last "penultimate" node, because
+                // the next one is the tail. 
+                // Assign the current node we are on in the loop to the secondtolast variable.
+                $secondtolast = $current;
+
+                break;
+
+            }
+
+            // if we aren't at the secondtolast node (because the current node's next property isnt
+            // the tail), we move on to check the next node in the linked list from the one we're on
+            // by moving our pointer to assign the next node to current.
+            $current = $current->next;
+
+        }
+
+        // set secondtolast.next to null as we are deleting the tail (its 'next' node)
+        $secondtolast->next = null;
+
+        // remove old tail by changing the tail to the secondtolast node.
+        $this->tail = $secondtolast;
+
+        // decrement the length of the linked list.
+        $this->length--;
+
+        return $node; // old tail node (???) why are we returning this?? Figure out.
+
     }
 
     /* delete a node */
